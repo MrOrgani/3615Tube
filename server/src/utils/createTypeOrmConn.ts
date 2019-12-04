0d41391a86c055f5b8e39efa92b255be82c7410d
Valentin LECOQ-VALLON <vlecoq-v@student.42.fr>
2019-12-04 11:27:52 +0100

    rm wrong name tests and utils unused

--- server/src/utils/createTypeOrmConn.ts
+++
@@ -1,9 +0,0 @@
-import { getConnectionOptions, createConnection } from "typeorm";
-
-export const createTypeOrmConn = async (connType: string) => {
-  // console.log(process.env.NODE_ENV);
-  // console.log(process.env);
-  const connectionOptions = await getConnectionOptions(connType);
-  console.log(connType);
-  return createConnection(connectionOptions);
-};
