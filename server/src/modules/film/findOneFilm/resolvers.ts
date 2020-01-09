41e8a13d3291af24315f52a58173e486b2bce6f4
Valentin LECOQ-VALLON <vlecoq-v@student.42.fr>
2020-01-09 18:12:58 +0100

    minor changes to graphql objects

--- server/src/modules/film/findOneFilm/resolvers.ts
+++ server/src/modules/film/findOneFilm/resolvers.ts
@@ -2,11 +2,6 @@ import { ResolverMap } from "../../../types/graphql-utils";
 import { createMiddleware } from "../../../utils/createMiddleware";
 import verifyAndSetSession from "../../middleware/verifyAndSetSession";
 import { pctAdd } from "../../../utils/apiGlobals";
-import Axios from "axios";
-import {
-  pctFormatFilmResult,
-  pctFormatTorrentsResult
-} from "../../../scripts/seedFilmDb/formats";
 import { Film } from "../../../entity/Films";
 
 const resolvers: ResolverMap = {
@@ -15,18 +10,10 @@ const resolvers: ResolverMap = {
       // findOneMoive: createMiddleware(
       //   verifyAndSetSession,
       async (_: any, { imdbId }: { imdbId: string }) => {
-        const url = `${pctAdd}/movie/${imdbId}`;
-        // console.log("in the findOneFilm Resolver", url);
         try {
           const result = (await Film.findOne({
             where: { imdbId: imdbId }
           })) as any;
-          // const pctFilm = await Axios.get(url);
-          // // console.log(pctFilm);
-          // if (!pctFilm.data) return null;
-          // const torrents = await pctFormatTorrentsResult(pctFilm.data);
-          // const result = await pctFormatFilmResult(pctFilm.data, torrents);
-          // // console.log(result);
           return result;
         } catch (err) {
           console.log("error in the film fetching", err);
