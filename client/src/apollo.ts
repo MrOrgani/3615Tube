2ec064bc974a627efe93c1f4fbec105d4f38aac8
Valentin LECOQ-VALLON <vlecoq-v@student.42.fr>
2020-01-17 14:51:50 +0100

    asdf

--- client/src/apollo.ts
+++ client/src/apollo.ts
@@ -1,6 +1,20 @@
-import ApolloClient from "apollo-boost";
+// import ApolloClient from "apollo-boost";
 
-export const client = new ApolloClient({
-  uri: "http://127.0.0.1:4000",
-  credentials: "include"
+// export const client = new ApolloClient({
+//   uri: "http://127.0.0.1:4000",
+//   credentials: "include"
+// });
+
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: "http://127.0.0.1:4000/"
+});
+
+export const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  link
 });
